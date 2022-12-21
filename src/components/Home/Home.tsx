f394190321c07ab4f3bd77398eb00d65df2ca369
Yulian Melnychuk <julian95@o2.pl>
2022-12-21 15:26:25 +0100

    update

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,10 @@
 import React,{useState} from 'react'
 import { useGetMoviesQuery } from '../../features/movies/movieApiSlice'
 import MovieCard from '../MovieCard/MovieCard'
-import NewPagination from '../Pagination/newPagination'
 import Pagination from '../Pagination/Pagination'
 import SearchBox from '../SearchBox/SearchBox'
 import Spinner from '../Spinner/Spinner'
+import { motion,AnimatePresence } from 'framer-motion'
 
 const Home = () => {
   const [searchTerm,setSearchTerm] =useState("")
@@ -16,7 +16,6 @@ const Home = () => {
   })
   // const [movies ,setMovies] =useState(data)
 
-  console.log(data)
 
   const searchMovies = data?.results?.filter((movie:any) => {
     if(!searchTerm.length) return movie
@@ -32,16 +31,20 @@ const Home = () => {
       <SearchBox setSearchTerm={setSearchTerm} />
     </div>
 
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mb-10 mt-10'>
-        {searchMovies && searchMovies.length ? (
+    <motion.div
+      layout
+      className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mb-10 mt-10'>
+        <AnimatePresence>
+          {searchMovies && searchMovies.length ? (
             searchMovies.map((movie:any) => {
               const { id, title, overview, poster_path,name } = movie;
               return <MovieCard key={id} id={id} title={title} name={name} overview={overview} poster_path={poster_path} />;
             })
-          ) : (
-            <h4>No Movie Found</h4>
-      )}
-    </div>
+            ) : (
+              <h4>No Movie Found</h4>
+              )}
+        </AnimatePresence>
+    </motion.div>
     <Pagination currentPage={setPage} page={page} pages={data?.total_pages}/>
     {/* <NewPagination currentPage={setPage} page={page} pages={data?.total_pages}/> */}
   </div>
--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,5 +1,9 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
+import { motion } from 'framer-motion'
+
+
+
 interface MovieCardProps {
   poster_path: string;
   overview?: string;
@@ -11,15 +15,21 @@ interface MovieCardProps {
 const MovieCard: FC<MovieCardProps> = (props) => {
   const { poster_path, overview, title, id,name} = props;
   return (
+  <motion.div
+      animate={{opacity:1}}
+      initial={{opacity:0}} 
+      exit={{opacity:0}}
+     layout
+  >
     <Link
       to={`/${id}`}
       onClick={() => {
         // console.log(id);
       }}
-    >
+      >
       <div
         className="max-w-sm bg-white mx-auto rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 hover:-translate-y-4 
-    duration-200 hover:animate-pulse cursor-pointer
+        duration-200 hover:animate-pulse cursor-pointer
     "
       >
         <img
@@ -27,7 +37,7 @@ const MovieCard: FC<MovieCardProps> = (props) => {
           src={`https://image.tmdb.org/t/p/original${poster_path}`}
           alt={title}
           className="rounded-t-lg h-80 w-full object-cover"
-        />
+          />
 
         <div className="p-5">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title ||name}</h5>
@@ -37,6 +47,8 @@ const MovieCard: FC<MovieCardProps> = (props) => {
         </div>
       </div>
     </Link>
+  </motion.div>
+
   );
 };
 
